a823c082db5caf32d25eaaa1aadda908890f538e
thabo012 <lethabokhope@gmail.com>
2022-09-12 05:47:03 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stuido.h>
+
+/**
+*main - print the string in the function
+*
+*Description: using the main function
+*this program prints "programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");return (0);
